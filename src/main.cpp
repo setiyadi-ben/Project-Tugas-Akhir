c9497f42a20bee8e49c2a7d453d057c2d9704011
setiyadi-ben <106580966+setiyadi-ben@users.noreply.github.com>
2022-12-07 17:09:11 +0700

    prog-2_gabung dht11 dan bh1750

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,111 @@
-// Example testing sketch for various DHT humidity/temperature sensors written by ladyada
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+/*
+IMPORTANT NOTES https://docs.platformio.org/en/latest/faq/ino-to-cpp.html
 
-#include "Arduino.h"
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
+CREATED BY SETIYADI_BEN ALIAS BENNY HARTANTO SETIYADI FROM POLITEKNIK NEGERI SEMARANG AS A PURPOSE FOR
+COMPLETING THESIS AKA "TUGAS AKHIR" FOR GET BASc IN TELECOMMUNICATION ENGINEERING.
 
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+IN ADDITION, THESE SCRIPTS LIKE SENSOR READINGS, TELEGRAM BOT, TIMING, .etc ARE MODIFIED FROM ORIGINAL SOURCE
+FROM GITHUB AND MENTIONED BELOW
+*/
 
-#include <Adafruit_Sensor.h>
+// Lib for communicating with ESP32 PlatformIO Framework
+#include <Arduino.h>
+
+// Import lib sensor DHT11
+/*
+- DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+- Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+- Example testing sketch for various DHT humidity/temperature sensors written by ladyada
+*/
+#include <DHT.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
-#define DHTPIN 4     // Digital pin connected to the DHT sensor 
+#define DHTPIN 4  // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
-// Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
-// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+// Import lib sensor BH1750
+#include <Wire.h>
+#include <BH1750.h>
+
+/*
+// Interfacing DHT Sensor
+- Connect pin 1 (on the left) of the sensor to +5V
+- NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+  to 3.3V instead of 5V!
+- Connect pin 2 of the sensor to whatever your DHTPIN is
+- Connect pin 4 (on the right) of the sensor to GROUND
+- Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
 // Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
+- Note that older versions of this library took an optional third parameter to
+  tweak the timings for faster processors.  This parameter is no longer needed
+  as the current DHT reading algorithm adjusts itself to work on faster procs.
+*/
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+// Initialize BH1750 sensor.
+BH1750 lightMeter;
+
+void setup(){
   Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
 
   dht.begin();
+
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  Wire.begin();
+  // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
+  // For Wemos / Lolin D1 Mini Pro and the Ambient Light shield use Wire.begin(D2, D1);
+
+  lightMeter.begin();
+
+  Serial.println(F("Penggabungan Sensor Reading DHT11 dan BH1750"));
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
+  float h = dht.readHumidity();
   // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  // float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t) /* || isnan(f) */) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
   // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
+  // float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  // float hic = dht.computeHeatIndex(t, h, false);
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
+  // Print sensor readings
+  Serial.print(F("Temperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("% "));
+  // Serial.print(f);
+  // Serial.print(F("°F  Heat index: "));
+  // Serial.print(hic);
+  // Serial.print(F("°C "));
+  // Serial.print(hif);
+  // Serial.println(F("°F"));
+
+  // Compute value of light intensity
+  float lux = lightMeter.readLightLevel();
+  // Print sensor readings
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(2000);
 }
